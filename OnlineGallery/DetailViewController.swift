b32c1f6f2c3f237acbeb19db6c1852913cb795f2
Cheeezcake <cheeezcake@yandex.ru>
2019-12-09 20:56:23 +0300

    Optimisation. SNEGOCHEK implemented. Pinch zooming like in Instagram on detail screen implemented.

--- OnlineGallery/DetailViewController.swift
+++ OnlineGallery/DetailViewController.swift
@@ -13,18 +13,26 @@ import RxSwift
 import RxAlamofire
 import Kingfisher
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, UIScrollViewDelegate {
     
     @IBOutlet weak var detImageView: UIImageView!
     
     @IBOutlet weak var detTitleLable: UILabel!
     
     @IBOutlet weak var detDescription: UITextView!
+    @IBOutlet weak var scrollView: UIScrollView! {
+        didSet {
+            scrollView.minimumZoomScale = 1
+            scrollView.maximumZoomScale = 5.0
+            scrollView.delegate = self
+        }
+    }
     
     var detImage: GalleryItem?
     var task: URLSessionDataTask!
     var disposeBag = DisposeBag()
     var image:UIImage? = nil
+    var originalImageSize = CGSize()
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -33,6 +41,7 @@ class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.originalImageSize = detImageView.frame.size
         self.detImageView.hero.id = String(self.detImage!.image.id)
         self.detImageView.image = image
         self.detImageView.kf.indicatorType = .activity
@@ -40,6 +49,13 @@ class DetailViewController: UIViewController {
         self.detDescription.text = detImage!.description
     }
     
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return detImageView
+    }
+    
+    func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
+        scrollView.zoomScale = 1
+    }
     
     func setTitle() {
         self.navigationController?.navigationBar.topItem?.title = " "
